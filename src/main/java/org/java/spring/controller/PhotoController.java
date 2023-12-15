fb7b7770705ffc1cea37303b17b6d921a536a300
marcobusacca <marcobusacca01@gmail.com>
2023-12-15 12:18:38 +0100

    [BACK-END] PhotoController.java method "getPhoto"

--- src/main/java/org/java/spring/controller/PhotoController.java
+++ src/main/java/org/java/spring/controller/PhotoController.java
@@ -2,19 +2,30 @@ package org.java.spring.controller;
 
 import java.util.List;
 
+import org.java.spring.db.pojo.Category;
 import org.java.spring.db.pojo.Photo;
+import org.java.spring.db.serv.CategoryService;
 import org.java.spring.db.serv.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class PhotoController {
 
 	@Autowired
 	private PhotoService photoService;
+	
+	@Autowired
+	private CategoryService categoryService;
 	
 	@GetMapping
 	public String getPhotos(Model model, @RequestParam(required = false) String name) {
@@ -27,5 +38,19 @@ public class PhotoController {
 		model.addAttribute("name", name == null ? "" : name);
 		
 		return "photos";
+	}
+	
+	@GetMapping("/photos/{id}")
+	public String getPhoto(Model model, @PathVariable int id) {
+		
+		Photo photo = photoService.findById(id);
+		
+		List<Category> categories = photo.getCategories();
+		
+		model.addAttribute("photo", photo);
+		
+		model.addAttribute("categories", categories);
+		
+		return "photo";
 	}
 }
