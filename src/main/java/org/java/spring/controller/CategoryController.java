7ba14c9d30bac8f9243d6e39b75435652d2b7145
marcobusacca <marcobusacca01@gmail.com>
2023-12-15 14:34:54 +0100

    [BACK-END] CategoryController.java method "createCategory"

--- src/main/java/org/java/spring/controller/CategoryController.java
+++ src/main/java/org/java/spring/controller/CategoryController.java
@@ -3,6 +3,7 @@ package org.java.spring.controller;
 import java.util.List;
 
 import org.java.spring.db.pojo.Category;
+import org.java.spring.db.pojo.Photo;
 import org.java.spring.db.serv.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,7 +16,7 @@ public class CategoryController {
 	@Autowired
     private CategoryService categoryService;
 	
-	@GetMapping
+	@GetMapping("/categories")
 	public String getCategories(Model model) {
 		
 		List<Category> categories = categoryService.findAll();
@@ -23,5 +24,15 @@ public class CategoryController {
 		model.addAttribute("categories", categories);
 		
 		return "categories";
+	}
+	
+	@GetMapping("/categories/create")
+	public String createCategory(Model model) {
+		
+		Category category = new Category();
+		
+		model.addAttribute("category", category);
+		
+		return "category-form";
 	}
 }
