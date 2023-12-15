52815c9c5df81c813a62c8d987d9b38dae3d5b4f
marcobusacca <marcobusacca01@gmail.com>
2023-12-15 14:29:22 +0100

    [BACK-END] CategoryController.java setup

---
+++ src/main/java/org/java/spring/controller/CategoryController.java
@@ -0,0 +1,17 @@
+package org.java.spring.controller;
+
+import java.util.List;
+
+import org.java.spring.db.pojo.Category;
+import org.java.spring.db.serv.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CategoryController {
+	
+	@Autowired
+    private CategoryService categoryService;
+}
