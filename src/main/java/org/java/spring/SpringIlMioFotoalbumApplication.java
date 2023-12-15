b5e2191060c7a7ce856b617ba5d3e6527d451374
marcobusacca <marcobusacca01@gmail.com>
2023-12-15 10:34:26 +0100

    [BACK-END] setup of MVC Auth

--- src/main/java/org/java/spring/SpringIlMioFotoalbumApplication.java
+++ src/main/java/org/java/spring/SpringIlMioFotoalbumApplication.java
@@ -1,13 +1,23 @@
 package org.java.spring;
 
+import java.time.LocalDate;
+
+import org.java.spring.auth.conf.AuthConf;
+import org.java.spring.auth.db.pojo.Role;
+import org.java.spring.auth.db.pojo.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringIlMioFotoalbumApplication {
+public class SpringIlMioFotoalbumApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIlMioFotoalbumApplication.class, args);
 	}
 
+	public void run(String... args) throws Exception {
+	
+		System.out.println("ok");
+	}
 }
